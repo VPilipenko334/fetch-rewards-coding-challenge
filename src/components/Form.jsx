8a153a8636e9ef635df4797261fb41d2f503f299
veronika <veronika.pilipenko@gmail.com>
2022-01-25 17:23:10 -0500

    success button works

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,8 +7,10 @@ const Form = () => {
         lastName: "",
         email: ""
     })
-    const [occupations, setOccupations] = useState('')
+    const [occupations, setOccupations] = useState('');
     const [states, setStates] = useState('');
+    const [success, setSuccess] = useState(false);
+    const [faliure, setFaliure] = useState(true);
 
     const url = 'https://frontend-take-home.fetchrewards.com/form'
 
@@ -21,22 +23,49 @@ const Form = () => {
         // .catch(error => console.log(error))
 
 
+        //handlers for every keystroke 
+       const handleFirstNameChange = (e) => {
+            setValues({...values, firstName: e.target.value})
+        }
+       const handleLastNameChange = (e) => {
+            setValues({...values, lastName: e.target.value})
+        }
+       const handleEmailChange = (e) => {
+            setValues({...values, email: e.target.value})
+        }
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            setSuccess(true);
+        }
+       
+       
+
     return (
         <div> 
             <h1 className="form-wrapper">Form:</h1>
                 <br/>
+
+            {success ? <p>Success! Thank you for registering!!!</p> : faliure }
+
             <div className="values-form-wrapper">
                     <input 
+                    value={values.firstName}
+                    onChange={handleFirstNameChange}
                     className="first-name"
                     placeholder="First Name"
                     name="firstName" />
                 <br /><br />
                     <input 
+                    value={values.lastName}
+                    onChange={handleLastNameChange}
                     className="last-name"
                     placeholder="Last Name"
                     name="lastName" />
                 <br /><br />
                     <input 
+                    value={values.email}
+                    onChange={handleEmailChange}
                     className="email"
                     placeholder="Email"
                     name="email" />
@@ -49,7 +78,10 @@ const Form = () => {
 
             <br/>
 
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
+
+            {/* {faliure ? <p>You cannot submit the form unless all the fields are filled out!</p> : success} */}
+
         </div>
     )
 }

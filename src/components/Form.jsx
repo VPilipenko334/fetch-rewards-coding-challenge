72c1c0c9fb5dc57fd7e1688d0373a1224841fb33
veronika <veronika.pilipenko@gmail.com>
2022-01-25 19:26:11 -0500

    fixing states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,34 +3,40 @@ import React, {useState, useEffect} from 'react';
 const Form = () => {
 
     const [values, setValues] = useState({
-        firstName: "",
+        fullName: "",
         email: "",
         password: ""
     })
-    const [occupations, setOccupations] = useState('');
-    const [states, setStates] = useState('');
+    const [occupations, setOccupations] = useState([]);
+    const [states, setStates] = useState([]);
     const [success, setSuccess] = useState(false);
     const [faliure, setFaliure] = useState(true);
 
     const url = 'https://frontend-take-home.fetchrewards.com/form'
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // setOccupations(data.occupations);
-            console.log(data)
-            console.log(data.states)
-            console.log(data.occupations)
 
-            // setStates(data.states);
-            // setOccupations(data.occupations);
-        })
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // setOccupations(data.occupations);
+                // console.log(data)
+                // console.log(data.states)
+                console.log(data.states)
+                // console.log(data.occupations)
+
+                setStates(data.states);
+                setOccupations(data.occupations);
+            })
+    }, [])
+        // empty array -> runs once when the component mounts
+    
         // .catch(error => console.log(error))
 
 
         //handlers for every keystroke 
-       const handleFirstNameChange = (e) => {
-            setValues({...values, firstName: e.target.value})
+       const handlefullNameChange = (e) => {
+            setValues({...values, fullName: e.target.value})
         }
  
        const handleEmailChange = (e) => {
@@ -56,11 +62,11 @@ const Form = () => {
 
             <div className="values-form-wrapper">
                     <input 
-                    value={values.firstName}
-                    onChange={handleFirstNameChange}
-                    className="first-name"
-                    placeholder="First Name"
-                    name="firstName" />
+                    value={values.fullName}
+                    onChange={handlefullNameChange}
+                    className="full-name"
+                    placeholder="Full Name"
+                    name="fullName" />
                 <br /><br />
                     <input 
                     value={values.email}
@@ -82,28 +88,20 @@ const Form = () => {
             <div className="dropdown-values">
                     <label className="occupations">Choose an Occupation:</label>
                     <select name="occupations" id="occupations">
-                        <option value="occupations"> hi
-                            {/* {occupations.map(occupation => <p>occupation</p>)} */}
-                        </option>
-                        
+                            {occupations.map(occupation => 
+                                <option value="occupations">{occupation}</option>)}
                     </select>
-                <br/><br/>
-                    <label className="states">Choose a State:</label>
-                    <select name="states" id="states">
-                        <option value="states"> hi
-                            {/* {states.map(state =>  <select value="states">state</select>)} */}
-                        </option>
-                    </select>
-                   
 
-                {/* {occupations}
-                {states} */}
+                    <br/><br/>
+
+                    <label className="states">Choose a State:</label>
+                    {/* <select name="states" id="states">
+                        {states.map(state =>
+                            <option value="states">{state}</option>)}
+                    </select> */}
             </div>  
 
             <div>
-                {/* {states.map(state => <p>state</p>)} */}
-                
-                {/* {states} */}
 
             </div>
 
